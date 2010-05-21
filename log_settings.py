7531d5cd3489bf9cdc72e9bf6e9c0046d8396740
Fred Wenzel <fwenzel@mozilla.com>
2010-05-21 19:19:23 +0200

    Django 1.2 project.

---
+++ log_settings.py
@@ -0,0 +1,34 @@
+import logging
+import logging.handlers
+
+from django.conf import settings
+
+
+# Loggers created under the "reporter" namespace, e.g. "reporter.caching", will
+# inherit the configuration from the base logger.
+log = logging.getLogger('reporter')
+
+level = settings.LOG_LEVEL
+
+if settings.DEBUG:
+    fmt = ('%(asctime)s %(name)s:%(levelname)s %(message)s '
+           ':%(pathname)s:%(lineno)s')
+    fmt = getattr(settings, 'LOG_FORMAT', fmt)
+    handler = logging.StreamHandler()
+    formatter = logging.Formatter(fmt, datefmt='%H:%M:%S')
+else:
+    fmt = '%s: %s' % (settings.SYSLOG_TAG,
+              '%(name)s:%(levelname)s %(message)s :%(pathname)s:%(lineno)s')
+    fmt = getattr(settings, 'SYSLOG_FORMAT', fmt)
+    SysLogger = logging.handlers.SysLogHandler
+    handler = SysLogger(facility=SysLogger.LOG_LOCAL7)
+    formatter = logging.Formatter(fmt)
+
+log.setLevel(level)
+handler.setLevel(level)
+handler.setFormatter(formatter)
+
+for f in getattr(settings, 'LOG_FILTERS', []):
+    handler.addFilter(logging.Filter(f))
+
+log.addHandler(handler)
---
+++ manage.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+import os
+import site
+import sys
+
+from django.core.management import execute_manager, setup_environ
+
+ROOT = os.path.dirname(os.path.abspath(__file__))
+path = lambda *a: os.path.join(ROOT, *a)
+
+site.addsitedir(path('apps'))
+#site.addsitedir(path('lib'))
+
+try:
+    import settings_local as settings
+except ImportError:
+    try:
+        import settings # Assumed to be in the same directory.
+    except ImportError:
+        import sys
+        sys.stderr.write(
+            "Error: Tried importing 'settings_local.py' and 'settings.py' "
+            "but neither could be found (or they're throwing an ImportError)."
+            " Please come back and try again later.")
+        raise
+
+# The first thing execute_manager does is call `setup_environ`. Logging config
+# needs to access settings, so we'll setup the environ early.
+setup_environ(settings)
+
+# Import for side-effect: configures our logging handlers.
+import log_settings
+
+
+if __name__ == "__main__":
+    execute_manager(settings)
