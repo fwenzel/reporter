5e9de46ccd124a0d5f4ab25c654b501dcc1a024a
Fred Wenzel <fwenzel@mozilla.com>
2010-05-26 11:16:33 +0200

    initial, mozilla.com-esque layout.

--- urls.py
+++ urls.py
@@ -1,14 +1,14 @@
 from django.conf.urls.defaults import *
+from django.views.generic.simple import direct_to_template
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Example:
-    # (r'^reporter/', include('reporter.foo.urls')),
+    (r'^$', direct_to_template, {'template': 'feedback/dashboard.html'}),
 
-    # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
+    # Uncomment the admin/doc line below and add 'django.contrib.admindocs'
     # to INSTALLED_APPS to enable admin documentation:
     # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
