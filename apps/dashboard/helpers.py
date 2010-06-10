0582917c4870efecb2f5c165a128a73ba614cca7
Fred Wenzel <fwenzel@mozilla.com>
2010-06-10 14:02:42 +0200

    search_url helper for building search URLs with default values.

---
+++ apps/dashboard/helpers.py
@@ -0,0 +1,23 @@
+from urllib import urlencode
+
+from django.core.urlresolvers import reverse
+
+from jingo import register
+import jinja2
+
+from feedback import FIREFOX
+
+@register.function
+def search_url(q=None, product=FIREFOX.short, version=None, locale=None,
+               os=None, date_start=None, date_end=None):
+    input = locals()
+
+    search = reverse('search')
+    data = {}
+
+    # set defaults
+    for (name, val) in input.items():
+        if val:
+            data[name] = val
+
+    return jinja2.Markup('%s?%s' % (search, urlencode(data)))
