140fe6d9475fd8d4189ee0f98a4e3410740264dc
Fred Wenzel <fwenzel@mozilla.com>
2010-06-10 18:58:30 +0200

    search results pagination (bug 570733)

---
+++ apps/search/helpers.py
@@ -0,0 +1,28 @@
+from urllib import urlencode
+
+from django.core.urlresolvers import reverse
+
+from jingo import register
+import jinja2
+
+from feedback import FIREFOX
+from .forms import ReporterSearchForm
+
+@register.function
+def search_url(defaults=None, extra=None, **kwargs):
+    search = reverse('search')
+    if not defaults:
+        defaults = {}
+    data = []
+
+    # get field data from keyword args or defaults
+    for field in ReporterSearchForm.base_fields:
+        val = kwargs.get(field, defaults.get(field, None))
+        if val:
+            data.append((field, unicode(val).encode('utf-8')))
+
+    # append extra fields
+    if extra:
+        data += extra.items()
+
+    return jinja2.Markup(u'%s?%s' % (search, urlencode(data)))
