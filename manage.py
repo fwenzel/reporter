ba08c2410e62f3ec5763a22610b92bc58b3069de
Fred Wenzel <fwenzel@mozilla.com>
2010-06-17 10:17:14 +0200

    fixing manage.py to work with vendor library

--- manage.py
+++ manage.py
@@ -3,20 +3,33 @@ import os
 import site
 import sys
 
-from django.core.management import execute_manager, setup_environ
 
 ROOT = os.path.dirname(os.path.abspath(__file__))
+
 path = lambda *a: os.path.join(ROOT, *a)
 
+prev_sys_path = list(sys.path)
+
 site.addsitedir(path('apps'))
-site.addsitedir(path('lib'))
+site.addsitedir(path('libs'))
 site.addsitedir(path('vendor'))
+
+# Move the new items to the front of sys.path. (via virtualenv)
+new_sys_path = []
+for item in list(sys.path):
+    if item not in prev_sys_path:
+        new_sys_path.append(item)
+        sys.path.remove(item)
+sys.path[:0] = new_sys_path
+
+# No third-party imports until we've added all our sitedirs!
+from django.core.management import execute_manager, setup_environ
 
 try:
     import settings_local as settings
 except ImportError:
     try:
-        import settings # Assumed to be in the same directory.
+        import settings
     except ImportError:
         import sys
         sys.stderr.write(
@@ -25,12 +38,9 @@ except ImportError:
             " Please come back and try again later.")
         raise
 
-# The first thing execute_manager does is call `setup_environ`. Logging config
+# The first thing execute_manager does is call `setup_environ`.  Logging config
 # needs to access settings, so we'll setup the environ early.
 setup_environ(settings)
-
-# Import for side-effect: configures our logging handlers.
-import log_settings
 
 
 if __name__ == "__main__":
