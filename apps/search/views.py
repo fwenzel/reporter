f8b3b2d780a35aadd96f102d39665d8aea873878
Fred Wenzel <fwenzel@mozilla.com>
2010-07-05 16:09:43 +0200

    re-added terms to search results (bug 576947)

--- apps/search/views.py
+++ apps/search/views.py
@@ -1,8 +1,12 @@
+from django.conf import settings
+
 import jingo
 
-from client import Client
-from forms import ReporterSearchForm
 from feedback import stats
+from feedback.models import Term
+
+from .client import Client
+from .forms import ReporterSearchForm
 
 
 def index(request):
@@ -23,5 +27,8 @@ def index(request):
         data['sent'] = stats.sentiment(qs=opinions)
         data['demo'] = stats.demographics(qs=opinions)
 
-    return jingo.render(request, 'search/search.html', data)
+        frequent_terms = opinions.terms.frequent()Term.objects.frequent().filter(
+            used_in__in=opinions)[:settings.TRENDS_COUNT]
+        data['terms'] = stats.frequent_terms(qs=frequent_terms)
 
+    return jingo.render(request, 'search/search.html', data)
