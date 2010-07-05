9cb8e71a08f8135de2ae0c4abb42fd7b06f074cd
Dave Dash <dd+github@davedash.com>
2010-07-05 12:55:16 -0700

    Undo search view changes.

--- apps/search/views.py
+++ apps/search/views.py
@@ -5,7 +5,7 @@ import jingo
 from view_cache_utils import cache_page_with_prefix
 
 from feedback import stats
-from feedback.models import Opinion, Term
+from feedback.models import Term
 
 from .client import Client, SearchError
 from .forms import ReporterSearchForm
@@ -23,31 +23,25 @@ def index(request):
 
     if form.is_valid():
         query = form.cleaned_data.get('q', '')
-        if not query:
-            # browse mode
-            opinions = Opinion.objects.browse(**form.cleaned_data)
-        else:
-            # search mode
-            search_opts = form.cleaned_data
-            try:
-                c = Client()
-                opinions = c.query(query, **search_opts)
-            except SearchError, e:
-                return jingo.render(request, 'search/unavailable.html',
-                                    {'search_error': e}, status=500)
+        search_opts = form.cleaned_data
+        try:
+            c = Client()
+            opinions = c.query(query, **search_opts)
+        except SearchError, e:
+            return jingo.render(request, 'search/unavailable.html',
+                                {'search_error': e}, status=500)
 
     else:
         query = ''
         opinions = None
 
     if opinions:
+        data['opinions'] = opinions
         data['sent'] = stats.sentiment(qs=opinions)
         data['demo'] = stats.demographics(qs=opinions)
 
         frequent_terms = Term.objects.frequent().filter(
             used_in__in=opinions)[:settings.TRENDS_COUNT]
         data['terms'] = stats.frequent_terms(qs=frequent_terms)
-        # TODO paginate instead of just limiting to 20!
-        data['opinions'] = opinions[:20]
 
     return jingo.render(request, 'search/search.html', data)
