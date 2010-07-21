946966a60b326f235e46e2538d48225b30c8526b
Fred Wenzel <fwenzel@mozilla.com>
2010-07-21 08:17:24 +0200

    added Greek, Portuguese

--- settings.py
+++ settings.py
@@ -74,7 +74,8 @@ USE_I18N = True
 USE_L10N = True
 
 # Accepted locales
-INPUT_LANGUAGES = ('cs', 'de', 'en-US', 'es-ES', 'fr', 'gl', 'it', 'pl',)
+INPUT_LANGUAGES = ('cs', 'de', 'el', 'en-US', 'es-ES', 'fr', 'gl', 'it', 'pl',
+                   'pt-PT')
 
 # Override Django's built-in with our native names
 class LazyLangs(dict):
