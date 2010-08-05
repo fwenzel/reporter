873eb7700bcabb7646dba80466ed11b9c0b2a4d2
michaelku <michael.kurze@rwth-aachen.de>
2010-08-04 23:29:38 -0700

    Use context manager for storage of processing results. Prepare to implement direct batch output.
    Explained how to generate sites.

--- apps/website_issues/management/commands/generate_sites.py
+++ apps/website_issues/management/commands/generate_sites.py
@@ -82,25 +82,21 @@ class Command(BaseCommand):
         err("Pregenerating positive/negative counts...\n")
         SiteGroup.calculate_positive_counts()
 
-        # Cascades to clusters and comments.
-        SiteSummary.objects.all().delete()
-
         # Generate IDs without hitting the database
-        self.site_summary_id = count()
-        self.cluster_id = count()
-        self.comment_id = count()
+        self.site_summary_id = count(1)
+        self.cluster_id = count(1)
+        self.comment_id = count(1)
+
+        err("Sorting...\n")
+        sorted_sites = SiteGroup.sorted(err)
 
         err("Generating clusters...\n")
-        try:
-            for i, group in enumerate(SiteGroup.sorted(err)):
-                if i % 200 == 0:
-                    err("...generated clusters for %i site summaries.\n" % i)
-                self.generate_clusters_for(err, group)
-        except:
-            err("An unexpected error occurred. Rolling back. Re-raising.\n")
-            transaction.rollback()
-            raise
-        else: transaction.commit()
+        with DatabaseStorage(err) as storage:
+            for i, group in enumerate(sorted_sites):
+                self.generate_clusters_for(err, storage, group)
+                # The first clusters take longest, report more often
+                if i < 100 or i % 100 == 99:
+                    err("...generated clusters for %i summaries\n" % (i+1))
         err("Generated clusters for %i site summaries.\n"
             % self.site_summary_id.next())
 
@@ -130,8 +126,7 @@ class Command(BaseCommand):
             if i % 1000 == 0: err("    ... %i comments\n" % i)
         err("%i site summaries for %i comments.\n" % (len(SiteGroup.all), i))
 
-
-    def add_singleton_cluster(self, site_summary, opinion):
+    def add_singleton_cluster(self, storage, site_summary, opinion):
         comment = Comment(pk=self.comment_id.next(),
                           description=opinion.description,
                           opinion_id=opinion.id,
@@ -141,23 +136,23 @@ class Command(BaseCommand):
                           primary_description=comment.description,
                           primary_comment=comment,
                           size=1)
-        cluster.save()
+        storage.save_cluster(cluster)
         comment.cluster = cluster
-        comment.save()
+        storage.save_comment(comment)
 
-    def generate_clusters_for(self, err, group):
+    def generate_clusters_for(self, err, storage, group):
         num_clusters = 0
         site_summary = SiteSummary(pk=self.site_summary_id.next(),
                                    size=len(group.opinion_pks),
                                    issues_count=group.positive_counts[0],
                                    praise_count=group.positive_counts[1],
                                    **group.key)
-        site_summary.save()
+        storage.save_site_summary(site_summary)
 
         # Handle single-comment case:
         if site_summary.size == 1:
             opinion = Opinion.objects.get(pk=group.opinion_pks[0])
-            self.add_singleton_cluster(site_summary, opinion)
+            self.add_singleton_cluster(storage, site_summary, opinion)
             return
 
         # Handle cluster case:
@@ -179,15 +174,52 @@ class Command(BaseCommand):
                               primary_description=comments[0].description,
                               primary_comment=comments[0],
                               size=len(comments))
-            cluster.save()
+            storage.save_cluster(cluster)
             for comment in comments:
                 del remaining_opinions[comment.opinion_id]
                 comment.cluster = cluster
-                comment.save()
+                storage.save_comment(comment)
 
         # Add singletons for remaining opinions
         for opinion in remaining_opinions.values():
-            self.add_singleton_cluster(site_summary, opinion)
+            self.add_singleton_cluster(storage, site_summary, opinion)
+
+
+class DatabaseStorage(object):
+    """Storage context that saves new model objects.
+
+    This storage inserts the objects into the database configured for their
+    model, replacing the current contents in one transaction.
+
+    Another possible storage might just directly generate a SQL script from
+    the objects it receives. That would be *much* faster as currently there is
+    one read and one write for every save with a pregenerated pk hogging the
+    tubes."""
+
+    def __init__(self, err):
+        self.err = err
+
+    def __enter__(self):
+        self.err("Deleting existing objects...\n")
+        # the delete cascades
+        SiteSummary.objects.all().delete()
+        return self
+
+    def __exit__(self, type, value, traceback):
+        if type is not None:
+            self.err("Unexpected error occurred. Rolling back, re-raising.\n")
+            transaction.rollback()
+            return False
+        transaction.commit()
+
+    def save_cluster(self, cluster):
+        cluster.save()
+
+    def save_site_summary(self, sitesummary):
+        sitesummary.save()
+
+    def save_comment(self, comment):
+        comment.save()
 
 
 class frozendict(dict):
