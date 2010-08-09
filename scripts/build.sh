d600255786e9c9e453f9af43406722454631228a
Dave Dash <dd+github@davedash.com>
2010-08-09 15:18:47 -0700

    Forgotten DB key.

--- scripts/build.sh
+++ scripts/build.sh
@@ -31,6 +31,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'mysql',
         'NAME': 'input_$1',
+        'PASSWORD': '',
         'OPTIONS': {'init_command': 'SET storage_engine=InnoDB'},
         'TEST_NAME': 'test_input_$1',
         'TEST_CHARSET': 'utf8',
