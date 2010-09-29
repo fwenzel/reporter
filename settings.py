f64414b022ff758655b38756646719c7e3cf612b
Dave Dash <dd+github@davedash.com>
2010-09-29 13:06:24 -0700

    CSRF fixes, bug 599999

--- settings.py
+++ settings.py
@@ -144,8 +144,8 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     'django.core.context_processors.debug',
     'django.core.context_processors.media',
     'django.core.context_processors.request',
-    'django.core.context_processors.csrf',
 
+    'csrf_context.csrf',
     'input.context_processors.i18n',
     'input.context_processors.input',
     'input.context_processors.mobile',
