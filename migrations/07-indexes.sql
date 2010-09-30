3eb25149f61775873848562fdb944997326d313a
Dave Dash <dd+github@davedash.com>
2010-09-30 10:14:24 -0700

    fix migration typo

--- migrations/07-indexes.sql
+++ migrations/07-indexes.sql
@@ -4,4 +4,4 @@ ALTER TABLE feedback_opinion ADD KEY `product` (`product`,`version`);
 ALTER TABLE feedback_opinion ADD KEY `cpv` (`created`, `product`, `version`);
 CREATE INDEX p ON theme (platform);
 CREATE INDEX np ON theme (num_opinions, product);
-CREATE INDEX ts ON theme (theme_id, score);
+CREATE INDEX ts ON theme_item (theme_id, score);
