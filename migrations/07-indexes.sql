ae65168a3369255670e3780d6609c739be5e23fe
Dave Dash <dd+github@davedash.com>
2010-09-30 09:49:02 -0700

    bug 600539, indexes

---
+++ migrations/07-indexes.sql
@@ -0,0 +1,7 @@
+ALTER TABLE feedback_opinion ADD KEY `cpvp` (`created`, `product`,`version`,
+`positive`);
+ALTER TABLE feedback_opinion ADD KEY `product` (`product`,`version`);
+ALTER TABLE feedback_opinion ADD KEY `cpv` (`created`, `product`, `version`);
+CREATE INDEX p ON theme (platform);
+CREATE INDEX np ON theme (num_opinions, product);
+CREATE INDEX ts ON theme (theme_id, score);
