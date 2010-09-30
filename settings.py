b9978e25555e249ce70788e703e05ff93118d524
Dave Dash <dd+github@davedash.com>
2010-09-30 13:54:12 -0700

    Don't use bad csrf middleware.

--- settings.py
+++ settings.py
@@ -167,7 +167,6 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
-    'django.middleware.csrf.CsrfResponseMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
 )
