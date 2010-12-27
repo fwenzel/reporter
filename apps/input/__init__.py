95ad5769bd4bb38263eacea81185b744f8c3bc33
Fred Wenzel <fwenzel@mozilla.com>
2010-12-27 11:26:36 -0800

    Updating known manufacturers and devices. Bug 621073.

--- apps/input/__init__.py
+++ apps/input/__init__.py
@@ -2,10 +2,21 @@ from tower import ugettext_lazy as _
 
 
 # Known manufacturers and devices for mobile feedback.
-KNOWN_MANUFACTURERS = ('Samsung', 'HTC', 'Motorola', 'LG', 'Sony')
-KNOWN_DEVICES = ('Hero', 'Vibrant', 'Droid', 'Epic', 'Evo', 'Desire',
-                 'Desire Z', 'Droid Incredible', 'Desire HD', 'EVO 4G',
-                 'Nexus One', 'Optimus Z', 'Droid 2', 'Droid X', 'Galaxy Tab')
+KNOWN_MANUFACTURERS = (
+    'Dell Inc.', 'HTC', 'LogicPD', 'Motorola', 'Nokia', 'Nokia', 'samsung',
+    'SHARP', 'Sony Ericsson', 'TOSHIBA',
+)
+KNOWN_DEVICES = (
+    'ADR6300', 'Dell Streak', 'Desire HD', 'Droid', 'DROID2', 'DROID2 GLOBAL',
+    'DROIDX', 'GT-I9000', 'GT-I9000M', 'GT-I9000T', 'GT-P1000', 'GT-P1000L',
+    'GT-P1000M', 'GT-P1000R', 'HD2 T8585', 'HTC bravo', 'HTC Desire',
+    'HTC Glacier', 'HTC HD2', 'HTC Vision', 'IS03', 'LogicPD Zoom2', 'MB520',
+    'MB860', 'Milestone', 'Nexus One', 'Nexus S', 'Nokia N900', 'PC36100',
+    'SAMSUNG-SGH-I897', 'SC-01C', 'SC-02B', 'SCH-I500', 'SCH-I800', 'SGH-T849',
+    'SGH-T959', 'SGH-T959D', 'SHW-M110S', 'SHW-M180S', 'SO-01B', 'SPH-D700',
+    'SPH-P100', 'T-Mobile G2', 'TOSHIBA_FOLIO_AND_A', 'VEGAn-TAB-v1.0.0b4',
+    'X10i',
+)
 
 # Stable Feedback: Rating Types
 class RATING_STARTUP:
