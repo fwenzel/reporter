5a27927ba9fc7cce18962e457bc42b9ef933b9d0
Fred Wenzel <fwenzel@mozilla.com>
2011-02-11 17:05:57 -0800

    Removed input.utils.uniquifier, as it is unused now. Bug 633664.

--- apps/input/utils.py
+++ apps/input/utils.py
@@ -1,25 +1,6 @@
 import zlib
 
 
-def uniquifier(seq, key=None):
-    """
-    Make a unique list from a sequence. Optional key argument is a callable
-    that transforms an item to its key.
-
-    Borrowed in part from http://www.peterbe.com/plog/uniqifiers-benchmark
-    """
-    if key is None:
-        key = lambda x: x
-    def finder(seq):
-        seen = {}
-        for item in seq:
-            marker = key(item)
-            if marker not in seen:
-                seen[marker] = True
-                yield item
-    return list(finder(seq))
-
-
 # TODO(davedash): liberate this
 def manual_order(qs, pks, pk_name='id'):
     """
