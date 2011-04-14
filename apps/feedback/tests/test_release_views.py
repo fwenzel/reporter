ff7fe4e19b3f3bd18c3591b7374c4bf2997b53a4
Dave Dash <dd+github@davedash.com>
2011-04-14 12:58:47 -0700

    bug 648405, Remove pre Fx4 tests

--- apps/feedback/tests/test_release_views.py
+++ apps/feedback/tests/test_release_views.py
@@ -1,15 +1,7 @@
-import json
-
 from nose import SkipTest
 from nose.tools import eq_
-from product_details.version_compare import Version
-from pyquery import PyQuery as pq
 
-from feedback.models import Opinion
-from input import (
-    FIREFOX, LATEST_RELEASE, MAX_FEEDBACK_LENGTH, MAX_IDEA_LENGTH,
-    OPINION_BROKEN, OPINION_IDEA, OPINION_RATING,
-    RATING_CHOICES, RATING_USAGE)
+from input import FIREFOX, LATEST_RELEASE
 from input.tests import FX_UA, ViewTestCase, enforce_ua
 from input.urlresolvers import reverse
 
@@ -47,15 +39,6 @@ class ReleaseTests(ViewTestCase):
         assert r['Location'].endswith(reverse('feedback.download',
                                               channel='release'))
 
-    # TODO bug 634324. Remove this after Firefox 4 release.
-    @enforce_ua
-    def test_old_release(self):
-        """Pre Fx4: Old release: redirect to beta download page."""
-        raise SkipTest
-        r = self._get_page('3.5')
-        eq_(r.status_code, 302)
-        assert r['Location'].endswith(reverse('feedback.download',
-                                              channel='beta'))
     # TODO bug 634324. Reenable this after Firefox 4 release.
     @enforce_ua
     def notest_latest_release(self):
@@ -63,28 +46,11 @@ class ReleaseTests(ViewTestCase):
         r = self._get_page(LATEST_RELEASE[FIREFOX])
         eq_(r.status_code, 200)
 
-    # TODO bug 634324. Remove this after Firefox 4 release.
-    @enforce_ua
-    def notest_latest_release(self):
-        """Pre Fx4: Firefox 4: no redirect."""
-        rel_ver = Version(LATEST_RELEASE[FIREFOX])
-        r = self._get_page('4.0' if Version('4.0') > rel_ver else
-                           rel_ver._version)
-        eq_(r.status_code, 200)
-
     @enforce_ua
     def test_newer_release(self):
         """Release version newer than current: no redirect."""
         r = self._get_page('20.0')
         eq_(r.status_code, 200)
-
-    @enforce_ua
-    def test_nightly(self):
-        """Nightly version: redirect."""
-        r = self._get_page('20.0b2pre')
-        eq_(r.status_code, 302)
-        assert r['Location'].endswith(reverse('feedback.download',
-                                              channel='beta'))
 
     def post_feedback(self, data, ajax=False, follow=True):
         """POST to the release feedback page."""
