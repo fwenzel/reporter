b28eedfb20567b32d6416385c30e83ee4449dbbc
Matthew Riley MacPherson <matt@lonelyvegan.com>
2011-05-25 17:50:56 -0300

    Update JS to use new tab markup; ignore window.location.hash

--- media/js/input-mobile.js
+++ media/js/input-mobile.js
@@ -5,42 +5,41 @@
                 return;
             }
 
-            $(this).toggleClass('collapsed')
+            $(this).toggleClass('collapsed');
             return false;
         });
     };
     $.fn.filters = function() {
-        this.find('input[type=checkbox]').click(doSubmit);
-        this.find('select').change(doSubmit);
-        return this;
-
         function doSubmit(e) {
             $(e.currentTarget).closest('form').submit();
             return false;
         }
+
+        this.find('input[type=checkbox]').click(doSubmit);
+        this.find('select').change(doSubmit);
+        return this;
     };
 
 
     $(document).ready(function() {
         $('.collapsible').collapsible();
         $('#filters').filters();
-
-        $('#header ul a').click(function(e) {
-            var clicked = $(this);
-            clicked.parent().siblings('li').find('a').each(function() {
-                var tohide = $(this);
-                tohide.removeClass('selected');
-                $(tohide.attr('href')).hide();
-            });
-            clicked.addClass('selected');
-            $(clicked.attr('href')).show();
-        }).first().click();
-
-        var hash = window.location.hash,
-            subpage = $('#header a[href$='+hash+']').first();
-        if (!subpage)
-            subpage = $('#header ul a:first');
-        subpage.click();
+		
+		$('#tabs a').live('click', function(e) {
+			$('.section').hide();
+			$($(this).attr('href')).show();
+			
+			$('#tabs a').removeClass('selected');
+			$(this).addClass('selected').blur();
+			
+			return false;
+		});
+		
+		// Activate the first tab
+		$('#tabs a:first').click();
+		
+		// Scroll past the browser's location bar in iOS/Firefox for Android
+		window.scrollTo(0, 1);
     });
 
 })(jQuery);
